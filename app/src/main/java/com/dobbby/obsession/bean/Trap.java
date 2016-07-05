8f01aeb0a76b76c3c51b8213eea7f19ccfec07f1
Dobbby <1321755451@qq.com>
2016-07-05 17:55:39 +0800

    Initial commit

--- app/src/main/java/com/dobbby/obsession/bean/Trap.java
+++ app/src/main/java/com/dobbby/obsession/bean/Trap.java
@@ -7,10 +7,22 @@ public class Trap {
     public static final int RECTANGLE = 0;
     public static final int TRIANGLE = 1;
     public static final int LEFT_TRIANGLE = 0;
-    public static final int RIGHT_TRANGLE = 1;
+    public static final int RIGHT_TRIANGLE = 1;
+    /**
+     * shape receive para: RECTANGLE or TRIANGLE.
+     */
     private int shape;
+    /**
+     * triangleDirection receive para: LEFT_TRIANGLE or RIGHT_TRIANGLE.
+     */
     private int triangleDirection;
+    /**
+     * width of the trap.
+     */
     private int width;
+    /**
+     * distance from the former trap.
+     */
     private int distance;
 
     public int getWidth() {

676333fa1fd7fdd4e15b3e25d6831c38ff3ef14d
Dobbby <1321755451@qq.com>
2016-07-05 19:34:55 +0800

    Initial commit

--- app/src/main/java/com/dobbby/obsession/activity/MainActivity.java
+++ app/src/main/java/com/dobbby/obsession/activity/MainActivity.java
@@ -2,16 +2,25 @@ package com.dobbby.obsession.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import com.dobbby.obsession.R;
 import com.dobbby.obsession.bean.GameCharacter;
 import com.dobbby.obsession.view.IShowGameView;
 
 public class MainActivity extends AppCompatActivity implements IShowGameView {
+    public float ρ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setΡ();
         setContentView(R.layout.activity_main);
+    }
+
+    private void setΡ() {
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        ρ = metrics.density;
     }
 
     @Override

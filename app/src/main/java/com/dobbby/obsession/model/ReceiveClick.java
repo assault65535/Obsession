9479a4c7b1232379b663612512cda7f922d3a438
assault65535 <814325852@qq.com>
2016-07-06 23:50:27 +0800

    small change

--- app/src/main/java/com/dobbby/obsession/model/ReceiveClick.java
+++ app/src/main/java/com/dobbby/obsession/model/ReceiveClick.java
@@ -8,7 +8,7 @@ public class ReceiveClick implements IReceiveClick {
     @Override
     public void judgeClick(final int clickPos, final OnUserClickListener listener) {
 
-        // 错误点击暂时用0代替, 正确点击暂时用1代替, 到时候再改 ...
+        //TODO 错误点击暂时用0代替, 正确点击暂时用1代替, 到时候再改 ...
         new Thread(new Runnable() {
             public void run() {
                 if (clickPos == 0) {
